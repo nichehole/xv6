567f32529a9be768357793faf28c650dafd44241
Chin Ho Lee <nichehole@gmail.com>
2013-11-29 15:47:30 -0500

    removed all

--- phil.d
+++
@@ -1,2 +0,0 @@
-phil.o: phil.c /usr/include/stdc-predef.h /usr/include/bits/predefs.h \
- types.h user.h thread.h
--- thread.d
+++
@@ -1,2 +0,0 @@
-thread.o: thread.c /usr/include/stdc-predef.h /usr/include/bits/predefs.h \
- types.h stat.h user.h thread.h
